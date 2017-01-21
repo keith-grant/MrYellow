efcdf03b5037980d0a8aabc2f3fcae00fd764792
KTNelson <kierantnelson@gmail.com>
2017-01-21 14:22:23 +0000

    added win lose stuff

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -16,62 +16,81 @@ func loadContent(filepath, array):
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	
+
 	loadContent("res://Docs/Questions.json", questionDictionary);
 	loadContent("res://Docs/Responses.json", responseDictionary);
-	
+
 	#for item in config:
 	#	print(config[item])
 	#	pass
-	
+
 	var mood = 50
 	var node = get_node("ProgressBar")
 	node.set_val(mood)
-	
+
 	var button_container = load("res://Scenes/ButtonContainer.tscn")
 	#var example_button_1 = {
 	#	"ResponseText" : "Option 1",
 	#	"ResultText" : "You picked option 1",
 	#	"ResultAudio" : "File 1"
 	#}
-	
+
 	#var example_button_2 = {
 	#	"ResponseText" : "Option 2",
 	#	"ResultText" : "You picked option 2",
 	#	"ResultAudio" : "File 2"
 	#}
-	
+
 	#var example_config = {
 	#	"button_1" : example_button_1,
 	#	"button_2" : example_button_2
 	#}
-	
+
 	var button_container_1 = button_container.instance()
 	button_container_1.init(questionDictionary["1"], responseDictionary)
 	add_child(button_container_1)
-	
+
 	set_fixed_process(true)
 	pass
 
 
 func _fixed_process(delta):
-	
+
 	var mood = get_node("MrYellow").get_mood()
-	
+
 	var node = get_node("ProgressBar")
 	node.set_val(mood)
-	
+
+func show_outcome():
+
+	do_props()
+	pass
+
+func do_props():
+	var mood = get_node("MrYellow").get_mood()
+	if(mood >= 100):
+		win()
+	elif (mood <= 0):
+		lose()
+	pass
+
 func ask_question():
 	randomize()
 	var new_id = randi()%11+1
 	var button_container = load("res://Scenes/ButtonContainer.tscn")
 	var bc_node = get_node("./ButtonContainer")
-	
+
 	var button_container_2 = button_container.instance()
 	bc_node.replace_by(button_container_2, false)
 	button_container_2.init(questionDictionary[str(new_id)], responseDictionary)
 	add_child(button_container_2)
+
+
+func win()
+	pass
 	
+func lose()
+	pass
 
 func _on_TestButton_pressed():
 	ask_question()
